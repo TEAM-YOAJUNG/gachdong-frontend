35712bbf15f7a1bfa48e83e36c1089d359695d3a
kang <kangju2000@gmail.com>
2024-11-02 16:31:10 +0900

    feat: prefetch hydarate

--- apps/client/app/Providers.tsx
+++ apps/client/app/Providers.tsx
@@ -3,7 +3,17 @@
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      staleTime: 0,
+      retry: false,
+    },
+  },
+});
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
